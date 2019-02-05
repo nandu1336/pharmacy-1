fd08a9864648e0b9133d8f9d1cdbd41253c3e6f0
sonzaiX <akshaykumar.02925@gmail.com>
2019-02-05 23:36:24 +0530

    Second commit - Modified forms.py to debug register attributes

--- bps/forms.py
+++ bps/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired,Email
+from wtforms.validators import DataRequired,Email,InputRequired,EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(),Email()])
@@ -8,10 +8,10 @@ class LoginForm(FlaskForm):
     rememberMe = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-# class RegisterForm(FlaskForm):
-# 	firstname = StringField('Firstname', validators=[DataRequired(),Email()]) 
-# 	lasttname = StringField('Firstname', validators=[DataRequired(),Email()]) 
-# 	email = StringField('Email', validators=[DataRequired(),Email()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
-#     submit = SubmitField('Sign In')
+class RegisterForm(FlaskForm):
+	firstname = StringField('Firstname', validators=[DataRequired()]) 
+	lasttname = StringField('Lastname', validators=[DataRequired()]) 
+	email = StringField('Email', validators=[DataRequired(),Email()])
+	password = PasswordField('New Password', [InputRequired(), EqualTo('confirm', message='Passwords must match')])
+	confirm  = PasswordField('Repeat Password')
+	submit = SubmitField('Sign Up')
