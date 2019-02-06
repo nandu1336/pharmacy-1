bec9fa3a6b3b837156d5862453a8ba572424edb1
sonzaiX <akshaykumar.02925@gmail.com>
2019-02-07 00:19:27 +0530

    Added dashboard.html and working.py

--- bps/__init__.py
+++ bps/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask,Blueprint
 from bps.admin.homepage import Home
 from bps.admin.account import Login,Register
+from bps.main.working import Dashboard
 from config import Config
 
 app = Flask(__name__)
@@ -9,3 +10,4 @@ app.config.from_object(Config)
 app.register_blueprint(Home, url_prefix='/')
 app.register_blueprint(Login, url_prefix='/')
 app.register_blueprint(Register, url_prefix='/')
+app.register_blueprint(Dashboard, url_prefix='/')
