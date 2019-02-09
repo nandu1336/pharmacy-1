5cafe4f4cefd4bf879db4823ddbd2b6541b2efd7
Nanda Kishore <nk732100@gmail.com>
2019-02-10 01:20:12 +0530

    Started the design of product info. file.But not much distiguishable from the dashboard page yet.

--- bps/main/working.py
+++ bps/main/working.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,redirect,flash,url_for,session
+from flask import Blueprint,render_template,redirect,flash,url_for,session,request
 
 Dashboard = Blueprint('Dashboard', __name__)
 
@@ -8,3 +8,14 @@ def dashboard():
 		return render_template("dashboard.html",title = 'Dashboard')
 	else:
 		return redirect(url_for('Login.login'))
+
+@Dashboard.route('/stock-Info')
+def stockInfo():
+	if 'user' in session :
+		if request.method == 'POST' and form.validate_on_submit():
+			return render_template('product.html')
+		else :
+			return render_template('product.html')
+	else :
+		return redirect(url_for('Login.login'))
+
